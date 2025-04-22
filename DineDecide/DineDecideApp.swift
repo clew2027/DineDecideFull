4531a0d448bf9854781820b6800df3bd9b029652
clew2027 <clew27@seas.upenn.edu>
2025-04-22 17:53:14 -0400

    Initial Commit

---
+++ DineDecide/DineDecideApp.swift
@@ -0,0 +1,17 @@
+//
+//  DineDecideApp.swift
+//  DineDecide
+//
+//  Created by Charlotte Lew on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct DineDecideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DineDecideTests/DineDecideTests.swift
@@ -0,0 +1,17 @@
+//
+//  DineDecideTests.swift
+//  DineDecideTests
+//
+//  Created by Charlotte Lew on 4/22/25.
+//
+
+import Testing
+@testable import DineDecide
+
+struct DineDecideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
